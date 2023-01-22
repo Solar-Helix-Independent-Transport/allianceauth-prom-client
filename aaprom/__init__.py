6b2585613898fb12c1693369c989086297a2daa6
Aaron Kable <aaronkable@gmail.com>
2023-01-22 16:26:00 +0800

    a4

--- aaprom/__init__.py
+++ aaprom/__init__.py
@@ -6,6 +6,6 @@ https://github.com/korfuri/django-prometheus
 """
 __all__ = ["middleware"]
 
-__version__ = "0.0.1a3"
+__version__ = "0.0.1a4"
 
 default_app_config = "aaprom.apps.AllianceAuthPrometheusConfig"
